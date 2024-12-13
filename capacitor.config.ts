110ae916d4e3a4534d8f178a9d74490fbd856ca3
Khosyinaa29 <khosyinuraliya@mail.ugm.ac.di>
2024-12-13 12:26:13 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carbonion',
+  webDir: 'www'
+};
+
+export default config;
